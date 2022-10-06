3b84ca4487da5ddc2bc722aca8cccb125e4f18cf
Siddarth Patel <siddarthpatel@Siddarths-MBP-2.lan1>
2022-10-06 01:03:38 -0400

    add 3 kafka broker initial setup

--- kafka-setup/setup.sh
+++ kafka-setup/setup.sh
@@ -1,5 +1,21 @@
-echo "Starting zookeeper and kafka conatiners"
-echo "Running docker-compose"
-docker-compose -f ./docker-compose.yml up -d
-echo "Listing running containers"
-docker ps
+echo "Enter an option (1 or 2) for the number of kafka brokers and zookeeper servers configuration"
+echo "1. ONE kafka broker and ONE zookeeper server config"
+echo "2. THREE kafka brokers and THREE zookeeper servers config"
+
+read varname
+
+if [[ "${varname}" == "1" ]];then
+    echo "ONE kafka broker architecture"
+    echo "Running docker-compose"
+    docker-compose -f ./docker-compose.yml up -d
+    echo "Listing running containers"
+    docker ps
+elif [[ "${varname}" == "2" ]];then
+    echo "THREE kafka broker architecture"
+    echo "Running docker-compose"
+    docker-compose -f ./docker-compose-2.yml up -d
+    echo "Listing running containers"
+    docker ps
+else
+    echo "invalid input"
+fi
