c191ec9388c13849a7d8a6f355e299a0141ae826
Siddarth Patel <siddarthpatel@Siddarths-MacBook-Pro-2.local>
2022-10-07 11:43:21 -0400

    add multi broker run instructions

--- kafka-setup/setup.sh
+++ kafka-setup/setup.sh
@@ -19,3 +19,20 @@ elif [[ "${varname}" == "2" ]];then
 else
     echo "invalid input"
 fi
+
+# multiple kafka brokers and zoopkers server config
+
+# docker exec -it  data-processing-kafka-1-1  /bin/sh
+# cd bin
+
+# list number of brokers with ids
+# zookeeper-shell localhost:22181 ls /brokers/ids
+
+# create topic with 5 partitions across 3 kafka brokers
+# kafka-topics --create --topic kafka_learning --replication-factor 1 --partitions 5 --bootstrap-server localhost:19092,localhost:29092,localhost:39092
+
+# start producer to write into any of the 3 kafka brokers
+# kafka-console-producer --topic kafka_learning --bootstrap-server localhost:19092,localhost:29092,localhost:39092
+
+# start consumer to read from any of the 3 kafka brokers
+# kafka-console-consumer --topic kafka_learning --bootstrap-server localhost:19092,localhost:29092,localhost:39092
